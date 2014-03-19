1aeeb896c20e697aacf2786597761a55e089a5b1
Pybreakyr <jawbreakyr@gmail.com>
2014-03-20 03:14:13 +0800

    registered the models to the admin site and set its value

--- practice/apps/articles/admin.py
+++ practice/apps/articles/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+
+from practice.apps.articles.models import Category
+from practice.apps.articles.models import Article
+
+
+
+admin.site.register(Category)
+admin.site.register(Article)
 # Register your models here.
+
+
+#from polls.models import Poll, Choice
+#
+#
+#class ChoiceInline(admin.TabularInline):
+#	model = Choice
+#	extra = 3
+#
+#class PollAdmin(admin.ModelAdmin):
+#	fieldsets = [
+#		('KIRVY', 			 {'fields': ['question']}),
+#		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#
+#	]
+	#inlines = [ChoiceInline]
+	#list_display = ('question', 'pub_date', 'was_published_recently')
+	#list_filter = ['pub_date']
+	#search_fields = ['question']
